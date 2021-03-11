8ea2e414713280bb77c23a42105dad9c9ad2b19d
tinkericlee <eric.xiangfu.lee@gmail.com>
2021-03-11 19:23:09 +0800

    Finish problem 35: use binary search

---
+++ solutions/array/easy/Search_Insert_Position.py
@@ -0,0 +1,30 @@
+# use binary search to locate the target possible index
+# if it is found, return the index
+# if it is not found, then separately check numbers in the range from start to end to determine its location
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        length = len(nums)
+
+        start, end = 0, length - 1
+
+        while end - start > 1:
+            mid = int((start + end) / 2)
+
+            if nums[mid] > target:
+                end = mid
+            elif nums[mid] < target:
+                start = mid
+            else:
+                return mid
+
+        if nums[start] == target:
+            return start
+        elif nums[end] == target:
+            return end
+        else:
+            if target <= nums[start]:
+                return start
+            elif target > nums[start] and target < nums[end]:
+                return start + 1
+            else:
+                return end + 1
