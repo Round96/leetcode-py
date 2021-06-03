11c5684f02f2ec1a625583fdc0f596b789fbaa0a
tinkericlee <eric.xiangfu.lee@gmail.com>
2021-06-03 15:11:07 +0800

    Finish Problem 53

---
+++ tests/array/easy/test_Maximum_Subarray.py
@@ -0,0 +1,9 @@
+from solutions.array.easy.Maximum_Subarray import Solution
+
+
+def test():
+    solution = Solution()
+    assert solution.maxSubArray([2, 3, 5, 1, 3]).__eq__(14)
+    assert solution.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]).__eq__(6)
+    assert solution.maxSubArray([1]).__eq__(1)
+    assert solution.maxSubArray([5, 4, -1, 7, 8]).__eq__(23)
